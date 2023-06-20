b1f98faf708ed77f2209c3e34a765c500d1003e3
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-20 17:12:31 -0500

    files&package

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+
+import  { Component } from "react";
+import { Searchbar } from '../Searchbar';
+import { ImageGallery } from '../ImageGallery/ImageGallery';
+import styles from './App.module.scss';
+
+export class App extends Component {
+  state = {
+    query: '',
+  }
+  
+  searchFormSubmitHandler = (query) => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div className={styles["app"]}>
+        <Searchbar onSubmit={this.searchFormSubmitHandler} />
+        <ImageGallery query={this.state.query} />
+        <h2>Designed by Emmanuel S Giraldo</h2>
+      </div>
+    );
+  }
+}
